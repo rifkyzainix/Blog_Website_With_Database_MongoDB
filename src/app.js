8c532d6d11f81eafd65e916c1147f9e8f7bfcd98
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-12-27 13:14:05 +0700

    Fix public

--- src/app.js
+++ src/app.js
@@ -15,7 +15,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"))
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+console.log(path.join(path.resolve(), "public"));
 
 mongoose.connect("mongodb+srv://zainifaroj:PgZCpjD4Ptub0DhS@cluster0.evsak56.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('strictQuery', true);
