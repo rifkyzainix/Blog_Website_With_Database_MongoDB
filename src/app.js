2ce927c1d9dcd0264b80ed3033ae993f2bd158ca
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-12-27 12:59:14 +0700

    Try fix path

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const contactContent = "Kami senang sekali mendengar dari Anda. Jika Anda memili
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(path.resolve(), "src/views"))
+app.set('views', path.join(__dirname, "src/views"))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
